84758214601b79021da7f2f49d664278cd65a007
Jango73 <guillaumed00@gmail.com>
2025-10-06 10:18:05 +0200

    Fix scripts

--- scripts/4-4-build.sh
+++ scripts/4-4-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export KERNEL_FILE="exos.bin"
+export FILE_SYSTEM=ext2
 make -j$(nproc)
--- scripts/4-8-build-vesa-test.sh
+++ scripts/4-8-build-vesa-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export PAYLOAD_SOURCE="test-payloads/vesa-test.c"
-export KERNEL_FILE="exos.bin"
 export DEBUG_OUTPUT=1
 export SCHEDULING_DEBUG_OUTPUT=0
 export TRACE_STACK_USAGE=0
+export KERNEL_FILE="exos.bin"
+export FILE_SYSTEM=ext2
+export PAYLOAD_SOURCE="test-payloads/vesa-test.c"
 make -j$(nproc)
