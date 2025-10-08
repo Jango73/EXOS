096a2953d04b7c7e5badd60ff2ff221bcccafc4e
Jango73 <Jango73@users.noreply.github.com>
2025-10-08 11:40:12 +0200

    Add initial x86-64 architecture header

---
+++ kernel/include/arch/x86-64/x86-64.h
@@ -0,0 +1,130 @@
+/************************************************************************\
+
+    EXOS Kernel
+    Copyright (c) 1999-2025 Jango73
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+    Intel x86-64 Architecture Support
+
+\************************************************************************/
+
+#ifndef ARCH_X86_64_X86_64_H_INCLUDED
+#define ARCH_X86_64_X86_64_H_INCLUDED
+
+/***************************************************************************/
+
+#include "arch/i386/I386.h"
+
+/***************************************************************************/
+
+#pragma pack(push, 1)
+
+/***************************************************************************/
+// General purpose register snapshot for 64-bit mode
+
+typedef struct tag_INTEL64GENERALREGISTERS {
+    U64 RFlags;
+    U64 RAX, RBX, RCX, RDX;
+    U64 RSI, RDI, RBP, RSP;
+    U64 R8, R9, R10, R11, R12, R13, R14, R15;
+    U64 RIP;
+    U16 CS, DS, SS;
+    U16 ES, FS, GS;
+    U64 CR0, CR2, CR3, CR4, CR8;
+    U64 DR0, DR1, DR2, DR3, DR6, DR7;
+} INTEL64GENERALREGISTERS, *LPINTEL64GENERALREGISTERS;
+
+/***************************************************************************/
+// IDT entry layout for 64-bit mode (16 bytes)
+
+typedef struct tag_X86_64_IDTENTRY {
+    U16 Offset_00_15;
+    U16 Selector;
+    U16 InterruptStackTable : 3;
+    U16 Reserved_0 : 5;
+    U16 Type : 4;
+    U16 Privilege : 2;
+    U16 Present : 1;
+    U16 Reserved_1 : 1;
+    U16 Offset_16_31;
+    U32 Offset_32_63;
+    U32 Reserved_2;
+} X86_64_IDTENTRY, *LPX86_64_IDTENTRY;
+
+/***************************************************************************/
+// System segment descriptor (e.g. TSS/LDT) layout for 64-bit mode (16 bytes)
+
+typedef struct tag_X86_64_SYSTEMSEGMENTDESCRIPTOR {
+    U16 Limit_00_15;
+    U16 Base_00_15;
+    U8 Base_16_23;
+    U8 Type : 4;
+    U8 Zero0 : 1;
+    U8 Privilege : 2;
+    U8 Present : 1;
+    U8 Limit_16_19 : 4;
+    U8 Available : 1;
+    U8 Zero1 : 2;
+    U8 Granularity : 1;
+    U8 Base_24_31;
+    U32 Base_32_63;
+    U32 Reserved;
+} X86_64_SYSTEMSEGMENTDESCRIPTOR, *LPX86_64_SYSTEMSEGMENTDESCRIPTOR;
+
+/***************************************************************************/
+// Generic 64-bit paging entry (PML4E, PDPTE, PDE, PTE)
+
+typedef struct tag_X86_64_PAGING_ENTRY {
+    U64 Present : 1;
+    U64 ReadWrite : 1;
+    U64 Privilege : 1;
+    U64 WriteThrough : 1;
+    U64 CacheDisabled : 1;
+    U64 Accessed : 1;
+    U64 Dirty : 1;
+    U64 PageSize : 1;
+    U64 Global : 1;
+    U64 Available_9_11 : 3;
+    U64 Address : 40;
+    U64 Available_52_58 : 7;
+    U64 Reserved_59_62 : 4;
+    U64 NoExecute : 1;
+} X86_64_PAGING_ENTRY, *LPX86_64_PAGING_ENTRY;
+
+/***************************************************************************/
+
+#pragma pack(pop)
+
+/***************************************************************************/
+// Extended control register helpers
+
+#define READ_CR8(var) __asm__ volatile("mov %%cr8, %0" : "=r"(var))
+#define WRITE_CR8(value) __asm__ volatile("mov %0, %%cr8" : : "r"(value) : "memory")
+
+/***************************************************************************/
+// Swap GS base in long mode
+
+#define SWAPGS() __asm__ volatile("swapgs" : : : "memory")
+
+/***************************************************************************/
+// Read and write RFLAGS using 64-bit instructions
+
+#define READ_RFLAGS64(var) __asm__ volatile("pushfq; pop %0" : "=r"(var))
+#define WRITE_RFLAGS64(value) __asm__ volatile("push %0; popfq" : : "r"(value) : "memory", "cc")
+
+/***************************************************************************/
+
+#endif
