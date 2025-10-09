67b3258184344acd424f8f1c224f4ccfba4cb785
Jango73 <Jango73@users.noreply.github.com>
2025-10-09 13:04:57 +0200

    Rename architecture include files

--- kernel/source/asm/RealModeCalls.asm
+++ kernel/source/asm/RealModeCalls.asm
@@ -22,7 +22,7 @@
 ;
 ;-------------------------------------------------------------------------
 
-%include "Kernel-i386.inc"
+%include "i386.inc"
 
 extern Kernel_i386
 extern SwitchToPICForRealMode
