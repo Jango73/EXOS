e876926c2b027db94571831d46245d903f56a47c
Jango73 <guillaumed00@gmail.com>
2025-10-09 20:41:46 +0200

    Add x86-64 scripts

---
+++ scripts/x86-64/4-4-build-release-ext2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export ARCH=x86-64
+export DEBUG_OUTPUT=0
+export SCHEDULING_DEBUG_OUTPUT=0
+export TRACE_STACK_USAGE=0
+export KERNEL_FILE="exos.bin"
+export FILE_SYSTEM=ext2
+make -j$(nproc)
