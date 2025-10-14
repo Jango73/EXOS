76514d1478a6e41bc60deb1e8e5511de397eb2d3
Jango73 <Jango73@users.noreply.github.com>
2025-10-14 08:34:22 +0200

    Fix varargs handling on x86-64

--- kernel/include/VarArg.h
+++ kernel/include/VarArg.h
@@ -25,10 +25,19 @@
 #ifndef VARARGS_H_INCLUDED
 #define VARARGS_H_INCLUDED
 
+// x86-64 builds rely on the compiler-provided vararg intrinsics to properly
+// handle register and stack based arguments following the System V ABI.
+#if defined(__x86_64__) || defined(__amd64__)
+typedef __builtin_va_list VarArgList;
+#define VarArgStart(ap, pn) __builtin_va_start(ap, pn)
+#define VarArg(ap, type) __builtin_va_arg(ap, type)
+#define VarArgEnd(ap) __builtin_va_end(ap)
+#else
 typedef LPSTR VarArgList[1];
 #define VA_ALIGN(sz) (((sz) + sizeof(int) - 1) & ~(sizeof(int) - 1))
 #define VarArgStart(ap, pn) ((ap)[0] = (LPSTR) & (pn) + VA_ALIGN(sizeof(pn)), (void)0)
 #define VarArg(ap, type) ((ap)[0] += VA_ALIGN(sizeof(type)), (*(type*)((ap)[0] - VA_ALIGN(sizeof(type)))))
 #define VarArgEnd(ap) ((ap)[0] = 0, (void)0)
+#endif
 
 #endif
