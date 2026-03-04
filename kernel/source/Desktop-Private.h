ad47d9590ebbca785c29164e1d2ac5063b5c33bf
Jango73 <guillaumed00@gmail.com>
2026-03-04 14:52:28 +0100

    refactor(kernel): split Desktop.c into desktop core and graphics units

---
+++ kernel/source/Desktop-Private.h
@@ -0,0 +1,63 @@
+/************************************************************************\
+
+    EXOS Kernel
+    Copyright (c) 1999-2025 Jango73
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+    Desktop private declarations
+
+\************************************************************************/
+
+#ifndef DESKTOP_PRIVATE_H_INCLUDED
+#define DESKTOP_PRIVATE_H_INCLUDED
+
+/************************************************************************/
+
+#include "GFX.h"
+
+/************************************************************************/
+
+extern BRUSH Brush_Desktop;
+extern BRUSH Brush_High;
+extern BRUSH Brush_Normal;
+extern BRUSH Brush_HiShadow;
+extern BRUSH Brush_LoShadow;
+extern BRUSH Brush_Client;
+extern BRUSH Brush_Text_Normal;
+extern BRUSH Brush_Text_Select;
+extern BRUSH Brush_Selection;
+extern BRUSH Brush_Title_Bar;
+extern BRUSH Brush_Title_Bar_2;
+extern BRUSH Brush_Title_Text;
+
+extern PEN Pen_Desktop;
+extern PEN Pen_High;
+extern PEN Pen_Normal;
+extern PEN Pen_HiShadow;
+extern PEN Pen_LoShadow;
+extern PEN Pen_Client;
+extern PEN Pen_Text_Normal;
+extern PEN Pen_Text_Select;
+extern PEN Pen_Selection;
+extern PEN Pen_Title_Bar;
+extern PEN Pen_Title_Bar_2;
+extern PEN Pen_Title_Text;
+
+BOOL ResetGraphicsContext(LPGRAPHICSCONTEXT This);
+
+/************************************************************************/
+
+#endif  // DESKTOP_PRIVATE_H_INCLUDED
